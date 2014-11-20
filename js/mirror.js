64bd6aa446a39c5f9426aafb0db8099477552ed2
j0shua <joshua.j.goldsmith@gmail.com>
2014-11-20 14:43:47 -0500

    fixed canvas to show video + flipped horizontal to act as a mirror

--- js/mirror.js
+++ js/mirror.js
@@ -9,10 +9,12 @@
 		video = document.querySelector('video'),
 		button = document.querySelector('button'),
 		canvas = document.querySelector('canvas'),
-		photo = document.querySelector('photo'),
+		photo = document.querySelector('img'),
 		width = 320,
 		height = 0,
-		isStreaming = false;
+		isStreaming = false,
+		context = canvas.getContext('2d');
+		interval = null;
 
 
 	function successCallback(stream){
@@ -23,13 +25,11 @@
 		}
 
 		video.play();
-
 	}
 
 	function errorCallback(err){
 		console.log('getUserMedia Errror', err);
 	}
-
 
 	// when ready attach video stream onto canvas element
 	video.addEventListener('canplay', function(ev){
@@ -37,15 +37,29 @@
 			height = video.videoHeight / (video.videoWidth/width)
 			video.setAttribute('width', width);
 			video.setAttribute('height', height);
+
 			canvas.setAttribute('width', width);
 			canvas.setAttribute('height', height);
+
+			context.translate(width, 0);
+      		context.scale(-1, 1);
 			isStreaming = true;
 		}
 	}, false);
 
+	video.addEventListener('play', function(ev){
+		if (video.paused || video.ended){
+			clearInterval(interval);
+		}
+		interval = setInterval(function(){
+			context.fillRect(0, 0, width, height);
+			context.drawImage(video, 0, 0, width, height);
+		}, 33);
+	}, false);
+
 	// onClick capture canvas as dataURL & set as img src attr
 	button.addEventListener('click', function(ev){
-		canvas.getContext('2d').drawImage(video, 0, 0, width, height);
+		context.drawImage(video, 0, 0, width, height);
 		photo.setAttribute('src', canvas.toDataURL('image/png'));
 
 		ev.preventDefault();
